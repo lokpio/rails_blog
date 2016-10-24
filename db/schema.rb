ffb7a2d1ef3297145c49aa523fca88b176125c3b
lokpio <lokpio322@gmail.com>
2016-10-23 21:38:53 -0700

    Change from sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161024032404) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
